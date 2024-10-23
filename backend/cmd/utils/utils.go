38b8778a7264a3de01989ca92cdfd4d0f47fbfb6
Harleyfliki <tomimikes@gmail.com>
2024-10-23 19:07:39 +0200

    feat: email sending function for testing

--- backend/cmd/utils/utils.go
+++ backend/cmd/utils/utils.go
@@ -4,6 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/smtp"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/miketsu-inc/reservations/backend/pkg/assert"
 )
 
 func ParseJSON(r *http.Request, data any) error {
@@ -21,4 +26,34 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 
 func WriteError(w http.ResponseWriter, status int, err error) {
 	WriteJSON(w, status, map[string]string{"error": err.Error()})
+}
+
+func SendMail(email string) error {
+	from := os.Getenv("EMAIL_ADDRESS")
+	password := os.Getenv("EMAIL_PASSWORD")
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort := os.Getenv("SMTP_PORT")
+	assert.True(from != "", "EMAIL_ADDRESS environment variable could not be found")
+	assert.True(password != "", "EEMAIL_PASSWORD environment variable could not be found")
+	assert.True(smtpPort != "", "SMTP_HOST environment variable could not be found")
+	assert.True(smtpPort != "", "SMTP_PORT environment variable could not be found")
+
+	to := []string{email}
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	msg := []byte("From: " + from + "\r\n" +
+		"To: " + email + "\r\n" +
+		"Subject: Email verification\r\n" +
+		"Content-Type: text/plain; charset=\"UTF-8\"\r\n" +
+		"\r\n" +
+		"CLick on this link to verify your email address: \r\n\n" +
+		"If it wasn't you who signed up with this email please ignore this message.")
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, []byte(msg))
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
