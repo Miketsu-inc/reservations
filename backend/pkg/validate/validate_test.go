2efa6b1fddc8d37b4fb012b2e207733fabe889ee
marce1l <mikesmarcell@gmail.com>
2024-11-10 22:53:46 +0100

    test: fix test expectation

--- backend/pkg/validate/validate_test.go
+++ backend/pkg/validate/validate_test.go
@@ -11,7 +11,7 @@ func TestMerchantNameToUrlName(t *testing.T) {
 	assert := assert.New(t)
 
 	testcases := []string{"lksgd*sdhf@hd#Kn8    88dlas", "áasdÜioű. --asdl7v 79(ÉÁ' asd) oav87pz", "Űő cxm,n76  jakl(*,.1)?"}
-	results := []string{"lksgd-sdhf-hd-Kn8-88dlas", "aasdUiou-asdl7v-79-EA-asd-oav87pz", "Uo-cxm-n76-jakl-1"}
+	results := []string{"lksgd-sdhf-hd-kn8-88dlas", "aasduiou-asdl7v-79-ea-asd-oav87pz", "uo-cxm-n76-jakl-1"}
 
 	for i, s := range testcases {
 		result, err := MerchantNameToUrlName(s)
