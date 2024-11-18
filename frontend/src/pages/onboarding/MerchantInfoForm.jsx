723d49581febb4e9487e7819581b69346fadc036
marce1l <mikesmarcell@gmail.com>
2024-11-19 00:26:41 +0100

    fix: useCallback eslint error

--- frontend/src/pages/onboarding/MerchantInfoForm.jsx
+++ frontend/src/pages/onboarding/MerchantInfoForm.jsx
@@ -97,7 +97,7 @@ export default function MerchantInfoForm({ isCompleted }) {
         url: "",
       });
     }
-  });
+  }, []);
 
   function handleInputData(data) {
     setFormData((prevAppData) => ({
