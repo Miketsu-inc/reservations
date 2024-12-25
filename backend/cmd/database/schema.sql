bd508eee7e795cf96c0bd74f491457dd452cdc5c
marce1l <mikesmarcell@gmail.com>
2024-12-26 00:57:39 +0100

    db: reorder for copy pasting, delete unused stuff

--- backend/cmd/database/schema.sql
+++ backend/cmd/database/schema.sql
@@ -1,6 +1,6 @@
 -- When modifying this always modify structs in the backend/database as well
 
-create table "User" (
+create table if not exists "User" (
     ID                      uuid            primary key unique not null,
     first_name              varchar(30)     not null,
     last_name               varchar(30)     not null,
@@ -8,12 +8,10 @@ create table "User" (
     phone_number            varchar(30)     not null,
     password_hash           varchar(72)     not null,
     jwt_refresh_version     integer         not null,
-    -- TODO
-    -- trial_ended     boolean         not null,
     subscription            integer         not null
 );
 
-create table "Merchant" (
+create table if not exists "Merchant" (
     ID                      uuid            primary key unique not null,
     name                    varchar(30)     not null,
     url_name                varchar(30)     unique not null,
@@ -26,21 +24,7 @@ create table "Merchant" (
     settings                jsonb
 );
 
-create table "Appointment" (
-    ID                      serial          primary key unique not null,
-    client_id               uuid            references "User" (ID) not null,
-    merchant_id             uuid            references "Merchant" (ID) not null,
-    service_id              integer         references "Service" (ID) not null,
-    location_id             integer         references "Location" (ID) not null,
-    from_date               timestamptz     not null,
-    to_date                 timestamptz     not null,
-    user_comment            text            default null,
-    merchant_comment        text            default null
-    -- TODO Possible future alternative
-    -- time_range      tstzrange       not null
-);
-
-create table "Service" (
+create table if not exists "Service" (
     ID                      serial          primary key unique not null,
     merchant_id             uuid            references "Merchant" (ID) not null,
     name                    varchar(30)     not null,
@@ -49,29 +33,23 @@ create table "Service" (
     blocking                boolean         not null
 );
 
--- TODO
--- create table "Employee" (
---     user_id         uuid            references "User" (ID) not null,
---     merchant_id     uuid            references "Merchant" (ID) not null,
---     location_id     integer         references "Location" (ID) not null
--- );
-
-create table "Location" (
+create table if not exists "Location" (
     ID                      serial          primary key unique not null,
     merchant_id             uuid            references "Merchant" (ID) not null,
     country                 varchar(50)     not null,
     city                    varchar(50)     not null,
     postal_code             varchar(10)     not null,
     address                 varchar(100)    not null
-    -- TODO
-    -- employees       varchar(1)      not null
 );
 
--- create table "Subscription" (
---     ID                      serial          primary key unique not null,
---     name                    varchar(30)     not null,
---     purchase_date           timestamptz     not null,
---     start_date              timestamptz     not null,
---     end_date                timestamptz     not null,
---     price_per_month         bigint          not null
--- );
+create table if not exists "Appointment" (
+    ID                      serial          primary key unique not null,
+    client_id               uuid            references "User" (ID) not null,
+    merchant_id             uuid            references "Merchant" (ID) not null,
+    service_id              integer         references "Service" (ID) not null,
+    location_id             integer         references "Location" (ID) not null,
+    from_date               timestamptz     not null,
+    to_date                 timestamptz     not null,
+    user_comment            text,
+    merchant_comment        text
+);
