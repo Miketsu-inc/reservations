aaf60d82a30473d0434efd947ab7fea716f64276
marce1l <mikesmarcell@gmail.com>
2024-12-29 00:28:34 +0100

    fix: tailwind classes for width and height instead of props, remove unnecessary styles

--- frontend/src/routes/signup/-components/SubmissionCompleted.jsx
+++ frontend/src/routes/signup/-components/SubmissionCompleted.jsx
@@ -2,11 +2,11 @@ import TickIcon from "@icons/TickIcon";
 
 export default function SubmissionCompleted({ text }) {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="my-4 mt-10 rounded-full border-4 border-green-600 p-6">
-        <TickIcon height="60" width="60" styles="fill-green-600" />
+    <div className="flex flex-col items-center gap-10">
+      <div className="my-4 mt-10 rounded-full border-4 border-green-600 p-4">
+        <TickIcon styles="w-16 w-16 fill-green-600" />
       </div>
-      <div className="mt-10 text-center text-xl font-semibold">{text}</div>
+      <p className="text-xl text-text_color">{text}</p>
     </div>
   );
 }
