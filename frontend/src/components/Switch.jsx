a8919b03933284193f7194f3ae1e89359b6626a4
marce1l <mikesmarcell@gmail.com>
2025-03-16 23:04:24 +0100

    feat: switch component

---
+++ frontend/src/components/Switch.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Switch({
+  size = "medium",
+  defaultValue = false,
+  variant = "default",
+  onSwitch,
+}) {
+  const [isOn, setIsOn] = useState(defaultValue);
+
+  return (
+    <button
+      aria-checked={isOn}
+      role="switch"
+      onClick={() => {
+        setIsOn(!isOn);
+        onSwitch();
+      }}
+      className={`${isOn ? `${variant === "monochrome" ? "bg-black dark:bg-white" : "bg-primary"}` : "bg-gray-300 dark:bg-gray-800"}
+        ${size === "small" ? "pr-4" : size === "medium" ? "pr-5" : size === "large" ? "pr-6" : ""}
+        cursor-pointer rounded-full py-1 pl-1 outline-gray-400 transition-colors
+        duration-100 focus-visible:outline-2 dark:outline-white`}
+    >
+      <div
+        className={`${isOn ? `${size === "small" ? "translate-x-3" : size === "medium" ? "translate-x-4" : size === "large" ? "translate-x-5" : ""}` : "translate-x-0"}
+          ${size === "small" ? "size-3" : size === "medium" ? "size-4" : size === "large" ? "size-5" : ""}
+          pointer-events-none rounded-full bg-white transition-transform duration-100
+          outline-none dark:bg-black`}
+      ></div>
+    </button>
+  );
+}
